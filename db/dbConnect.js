3bf979771c5dc9e3eec3e5c9e716110840a3638f
Karanveer Singh <singhkaranveer671@gmail.com>
2020-12-23 20:30:28 -0800

    Testing heroku knex setup

--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,24 +1,28 @@
 const knex = require("knex");
 require("dotenv").config();
-// let db;
-// if (process.env.NODE_ENV !== "production") {
-// 	db = knex({
-// 		client: process.env.DB_CLIENT,
-// 		connection: {
-// 			host: process.env.DATABASE_URL,
-// 			user: process.env.DB_USER,
-// 			password: process.env.DB_PASSWORD,
-// 			database: process.env.DB_NAME,
-// 		},
-// 	});
-// } else {
-	const db = knex({
+let db;
+if (process.env.NODE_ENV !== "production") {
+	db = knex({
+		client: process.env.DB_CLIENT,
+		connection: {
+			host: process.env.DATABASE_URL,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
+			ssl:true
+		},
+	});
+} else {
+	db = knex({
 		client: process.env.DB_CLIENT,
 		connection: {
 			connectionString: process.env.DATABASE_URL,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASSWORD,
+			database: process.env.DB_NAME,
 			ssl: true,
 		},
 	});
-// }
+}
 
 module.exports = db;

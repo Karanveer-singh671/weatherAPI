d807eb7f0d6ef3fbe531bee58b52dc273fd8b4c7
Karanveer Singh <singhkaranveer671@gmail.com>
2020-12-23 20:58:41 -0800

    adding config var

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const app = require("./app");
 
 /** env checks */
-if (!process.env.DATABASE_URL) {
-	throw new Error("DATABASE_URL must be defined");
-}
-if (!process.env.DB_NAME) {
-	throw new Error("DB_NAME must be defined");
-}
-if (!process.env.DB_PASSWORD) {
-	throw new Error("DB_PASSWORD must be defined");
-}
-if (!process.env.DB_USER) {
-	throw new Error("DB_USER must be defined");
-}
-if (!process.env.API_KEY) {
-	throw new Error("API_KEY must be defined");
-}
-if (!process.env.API_BASE_URL) {
-	throw new Error("API_BASE_URL must be defined");
+if (process.env.NODE_ENV !== "production") {
+	if (!process.env.DB_CLIENT) {
+		throw new Error("DB_CLIENT must be defined");
+	}
+	if (!process.env.DATABASE_URL) {
+		throw new Error("DATABASE_URL must be defined");
+	}
+	if (!process.env.DB_NAME) {
+		throw new Error("DB_NAME must be defined");
+	}
+	if (!process.env.DB_PASSWORD) {
+		throw new Error("DB_PASSWORD must be defined");
+	}
+	if (!process.env.DB_USER) {
+		throw new Error("DB_USER must be defined");
+	}
+	if (!process.env.API_KEY) {
+		throw new Error("API_KEY must be defined");
+	}
+	if (!process.env.API_BASE_URL) {
+		throw new Error("API_BASE_URL must be defined");
+	}
 }
 app.listen(process.env.PORT || 5000, () => {
 	console.log(`app is running on port ${process.env.PORT}`);
